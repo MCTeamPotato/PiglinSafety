72f99fc7b679f6fddaf214c137ec57e0075e2d5f
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2025-01-10 22:10:35 +0800

    Delete src/main directory

--- src/main/java/com/teampotato/piglinsafety/PiglinSafety.java
+++
@@ -1,8 +0,0 @@
-package com.teampotato.piglinsafety;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(PiglinSafety.MOD_ID)
-public class PiglinSafety {
-    public static final String MOD_ID = "piglinsafety";
-}
